6e7eafa23ebd3dba7f1aa89f81e726630a9797a4
Yann Herklotz <git@yannherklotz.com>
2020-08-16 16:07:19 +0100

    Add Package header

--- zettelkasten.el
+++ zettelkasten.el
@@ -1,23 +1,29 @@
-;;; Zettelkasten --- Helper functions to organise notes.
+;;; zettelkasten.el --- Helper functions to organise notes.
+
+;; Author: Yann Herklotz
+;; Keywords: notes
+;; Package-Requires: ((emacs "24.1"))
+;; Version: 1.0.0
+
 ;;; Commentary:
-;;;
-;;; Used to organise notes using the Zettelkasten method.
-;;;
-;;; Copyright (C) 2020  Yann Herklotz
-;;;
-;;; This program is free software: you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation, either version 3 of the License, or
-;;; (at your option) any later version.
-;;;
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-;;;
+
+;; Used to organise notes using the Zettelkasten method.
+;;
+;; Copyright (C) 2020  Yann Herklotz
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Code:
 
 (defgroup zettelkasten nil
